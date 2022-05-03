d702773f46e31533f433a387cb57ddf4fb271726
InsertyEXE <insertyend@gmail.com>
2022-05-03 18:39:22 -0300

    First commit

---
+++ app/src/main/java/com/example/climatyweather/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.climatyweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import com.example.climatyweather.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*
+        Tela para pesqusiar cidades
+        tela para mostras os status local
+        Lista de locais favoritos
+         */
+    }
+}
