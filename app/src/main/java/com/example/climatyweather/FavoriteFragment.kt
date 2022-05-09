8c3532b76847e2cab80e3e17a4074f38e02d89fb
InsertyEXE <insertyend@gmail.com>
2022-05-09 03:44:55 -0300

    feat: add favorite fragment
    Home icons changed and favorite fragment added

---
+++ app/src/main/java/com/example/climatyweather/FavoriteFragment.kt
@@ -0,0 +1,17 @@
+package com.example.climatyweather
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.climatyweather.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment: Fragment(R.layout.fragment_favorite) {
+
+    private var binding: FragmentFavoriteBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentFavoriteBinding.bind(view)
+    }
+}
