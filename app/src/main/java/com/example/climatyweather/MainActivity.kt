33f0ff33acc13446339e9df6a12810722cc14883
InsertyEXE <insertyend@gmail.com>
2022-05-10 02:35:21 -0300

    feat: replace fragment implemented
    already can replace fragment touching in nav bottom

--- app/src/main/java/com/example/climatyweather/MainActivity.kt
+++ app/src/main/java/com/example/climatyweather/MainActivity.kt
@@ -2,21 +2,76 @@ package com.example.climatyweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.example.climatyweather.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 private lateinit var binding: ActivityMainBinding
+private lateinit var homeFragment: HomeFragment
+private lateinit var searchFragment: SearchFragment
+private lateinit var favoriteFragment: FavoriteFragment
 
-class MainActivity : AppCompatActivity() {
+private var currentFragment: Fragment? = null
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*
-        Tela para pesqusiar cidades
-        tela para mostras os status local
-        Lista de locais favoritos
-         */
+        homeFragment = HomeFragment()
+        searchFragment = SearchFragment()
+        favoriteFragment = FavoriteFragment()
+
+        binding.mainNavBottom.setOnNavigationItemSelectedListener(this)
+        binding.mainNavBottom.selectedItemId = R.id.menu_home
+
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.menu_home ->{
+                if(currentFragment == homeFragment) return false
+                currentFragment = homeFragment
+            }
+            R.id.menu_search ->{
+                if(currentFragment == searchFragment) return false
+                currentFragment = searchFragment
+            }
+            R.id.menu_fav ->{
+                if(currentFragment == favoriteFragment) return false
+                currentFragment = favoriteFragment
+            }
+        }
+
+        currentFragment?.let {
+            replaceFragment(R.id.fragment, it)
+        }
+
+        return true
+    }
+
+
+    private fun replaceFragment(id: Int, fragment: Fragment){
+
+        if (supportFragmentManager.findFragmentById(id) == null){
+            supportFragmentManager.beginTransaction().apply {
+                add(id, fragment)
+                commit()
+            }
+        }
+        else{
+            supportFragmentManager.beginTransaction().apply {
+                replace(id, fragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+    }
+
 }
